d6d4b73ee911236496ba5d52a08145705c34acc0
ibrahimadle2220 <ibrahimadel2220@gamil.com>
2022-12-31 14:49:51 +0200

    Finish Search App For Some pokemon with it's details

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,8 @@
 import './App.css'
-// import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// import Dashboard from './pages/Dashboard'
-// import Products from './pages/Products'
-// import Categories from './pages/Categories'
-// import Users from './pages/Users'
-// import Admin from './pages/Admin'
-// import Orders from './pages/Orders'
-// import Error from './pages/Error'
-// import Login from './pages/Login'
-// import SharedLayout from './pages/SharedLayout'
-// import CodeCamp from './freecodecamp/CodeCamp'
 import Pokemon from './Tryagain/Pokemon'
-
 function App() {
   return (
     <Pokemon />
-      // <BrowserRouter>
-      //   <Routes>
-      //     <Route path='/' element={<SharedLayout />}>
-      //       <Route index element={<Dashboard />} />
-      //       <Route path='products' element={<Products />} />
-      //       <Route path='categories' element={<Categories />}/>
-      //       <Route path='users' element={<Users />}/>
-      //       <Route path='admins' element={<Admin />}/>
-      //       <Route path='orders' element={<Orders />}/>
-      //       <Route path='login' element={<Login />}/>
-      //       <Route path='*' element={<Error />}/>
-      //     </Route>
-      //   </Routes>
-      // </BrowserRouter>
   )
 }
-
 export default App
